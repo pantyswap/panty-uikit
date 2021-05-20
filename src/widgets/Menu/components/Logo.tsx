e05ee88b73bbb1f367801a052042061a82c5ad78
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-20 00:00:28 -0300

    Feat: update logo

--- src/widgets/Menu/components/Logo.tsx
+++ src/widgets/Menu/components/Logo.tsx
@@ -22,13 +22,13 @@ const StyledLink = styled(Link)`
   display: flex;
   align-items: center;
   .mobile-icon {
-    width: 32px;
+    width: 52px;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: none;
     }
   }
   .desktop-icon {
-    width: 160px;
+    width: 52px;
     display: none;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: block;
